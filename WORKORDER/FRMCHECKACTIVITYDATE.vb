e87e019cea872e2826e0a7f9fb2602573629266f
PUTCHaztig <putchaztig@gmail.com>
2021-11-24 16:05:13 +0800

    putch-11/24/2021-4:00 pm

---
+++ WORKORDER/FRMCHECKACTIVITYDATE.vb
@@ -0,0 +1,32 @@
+﻿Public Class FRMCHECKACTIVITYDATE
+    Private Sub FRMCHECKACTIVITYDATE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = Format(Now, "Short date")
+
+        Dim sql As String = ""
+
+
+        sql = <s>
+        SELECT WorkOrderCode, ActivityDate, IsActive, IsClose
+        FROM  dbo.t_FarmActivityWorkOrder
+        WHERE (IsActive = 1) AND (IsClose = 0) and WorkOrderCode >='<%= TextBox1.Text %>'
+
+              </s>
+
+        SelectQuery(sql)
+        With grid1
+            .DataSource = ds
+            .DataMember = "table"
+            .Rows(0).Height = 50
+            .Cols.Fixed = 0
+            '.Cols("RECID").Caption = "ID"
+            .AutoSizeCols()
+
+        End With
+
+
+    End Sub
+
+    Private Sub BtAdd_Click(sender As Object, e As EventArgs) Handles btAdd.Click
+
+    End Sub
+End Class
--- WORKORDER/My Project/Application.Designer.vb
+++ WORKORDER/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WORKORDER.FRMWORKORDERDASHBOARD
+            Me.MainForm = Global.WORKORDER.FRMCHECKACTIVITYDATE
         End Sub
     End Class
 End Namespace
